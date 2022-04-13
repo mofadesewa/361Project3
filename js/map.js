7d0b6971a4cd455289b86694ec90ee87c04065c9
Mofadesewa Atanda <44592706+mofadesewa@users.noreply.github.com>
2022-04-12 20:36:17 -0500

    Updating Feature 2
    Adding code to get map feature 2 working. Adding animation to the feature.

--- js/map.js
+++ js/map.js
@@ -22,25 +22,50 @@ function initMap() {
         position: Lagloc,
         map,
         icon: 'https://maps.google.com/mapfiles/kml/pal4/icon49.png',
+        /*animation: google.maps.Animation.BOUNCE */
+      });
+    
+    var Lagtext =
+    '<p>Lagos, most populous city</p>';
+    var Laginfo = new google.maps.InfoWindow({
+    content: Lagtext,
+    });
+    
+    LagMarker.addListener('mouseover', function () {
+    Laginfo.open(map, this);
+    });
+    LagMarker.addListener('mouseout', function () {
+    Laginfo.close();
+    });
+    
+    google.maps.event.addListener(LagMarker, 'click', function () {
+    map.setCenter(LagMarker.getPosition());
+    map.setZoom(14);
+    });
+
+    var AbjMarker = new google.maps.Marker({
+        position: Abjloc,
+        map,
+        icon: 'https://maps.google.com/mapfiles/kml/pal4/icon47.png',
         animation: google.maps.Animation.BOUNCE
       });
+
+    var Abjtext =
+    '<p>Abuja, capital</p>';
+    var Abjinfo = new google.maps.InfoWindow({
+    content: Abjtext,
+    });
     
-      var Lagtext =
-        '<p>Lagos, most populous city</p>';
-      var Laginfo = new google.maps.InfoWindow({
-        content: Lagtext,
-      });
+    AbjMarker.addListener('mouseover', function () {
+    Abjinfo.open(map, this);
+    });
+    AbjMarker.addListener('mouseout', function () {
+    Abjinfo.close();
+    });
     
-      LagMarker.addListener('mouseover', function () {
-        Laginfo.open(map, this);
-      });
-      LagMarker.addListener('mouseout', function () {
-        Laginfo.close();
-      });
-    
-      google.maps.event.addListener(LagMarker, 'click', function () {
-        map.setCenter(LagMarker.getPosition());
-        map.setZoom(14);
-      });
+    google.maps.event.addListener(AbjMarker, 'click', function () {
+    map.setCenter(AbjMarker.getPosition());
+    map.setZoom(14);
+    });
 }
 google.maps.event.addDomListener(window, 'load', initMap);
